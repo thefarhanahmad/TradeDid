7a934284a61f9c682bf4b77abe385252e72433db
farhan <akhtarfarhan281@gmail.com>
2024-10-30 13:09:25 +0530

    solved logo error

--- src/Components/Integrations/PartnershipUI.jsx
+++ src/Components/Integrations/PartnershipUI.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../../assets/logo.png";
+import Logo from "../../assets/Logo.png";
 
 const PartnershipUI = ({ companyName, CompanyLogo }) => {
   return (
